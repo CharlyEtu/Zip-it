f1098f08ab0e38095e9278d97bffab0c1a5c22ea
HABCHI <hamza0habchi@gmail.com>
2015-05-23 17:39:36 +0200

    Manage burger menu

--- Zipit/app/src/main/java/com/capella/zipit/activity/Menu_activity.java
+++ Zipit/app/src/main/java/com/capella/zipit/activity/Menu_activity.java
@@ -4,7 +4,11 @@ package com.capella.zipit.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,6 +17,7 @@ import android.widget.Button;
 import android.widget.Toolbar;
 
 import com.capella.zipit.R;
+import com.capella.zipit.adapter.MyAdapter;
 
 
 /**
@@ -23,6 +28,25 @@ import com.capella.zipit.R;
  * compressé (compression de chaine  de caracteres). 
  */
 public class Menu_activity extends ActionBarActivity {
+
+	//First We Declare Titles And Icons For Our Navigation Drawer List View
+	//This Icons And Titles Are holded in an Array as you can see
+
+	String TITLES[] = {"Repository","Settings","Infos"};
+	int ICONS[] = {R.drawable.ic_repo,R.drawable.ic_settings,R.drawable.ic_info};
+
+	RecyclerView mRecyclerView;                           // Declaring RecyclerView
+	RecyclerView.Adapter mAdapter;                        // Declaring Adapter For Recycler View
+	RecyclerView.LayoutManager mLayoutManager;            // Declaring Layout Manager as a linear layout manager
+	DrawerLayout Drawer;                                  // Declaring DrawerLayout
+
+	ActionBarDrawerToggle mDrawerToggle;
+
+	//Similarly we Create a String Resource for the name and email in the header view
+	//And we also create a int resource for profile picture in the header view
+
+	String NAME = "Habchi Hamza";
+	String EMAIL = "hamza0habchi@gmail.com";
 
 	private android.support.v7.widget.Toolbar toolbar;
 
@@ -51,6 +75,45 @@ public class Menu_activity extends ActionBarActivity {
 		toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.tool_bar);
 		setSupportActionBar(toolbar);
 
+
+		mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView); // Assigning the RecyclerView Object to the xml View
+
+		mRecyclerView.setHasFixedSize(true);                            // Letting the system know that the list objects are of fixed size
+
+		mAdapter = new MyAdapter(TITLES,ICONS,NAME,EMAIL);       // Creating the Adapter of MyAdapter class(which we are going to see in a bit)
+		// And passing the titles,icons,header view name, header view email,
+		// and header view profile picture
+
+		mRecyclerView.setAdapter(mAdapter);                              // Setting the adapter to RecyclerView
+
+		mLayoutManager = new LinearLayoutManager(this);                 // Creating a layout Manager
+
+		mRecyclerView.setLayoutManager(mLayoutManager);                 // Setting the layout Manager
+
+
+		Drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);        // Drawer object Assigned to the view
+		mDrawerToggle = new ActionBarDrawerToggle(this,Drawer,toolbar,R.string.openDrawer,R.string.closeDrawer){
+
+			@Override
+			public void onDrawerOpened(View drawerView) {
+				super.onDrawerOpened(drawerView);
+				// code here will execute once the drawer is opened( As I dont want anything happened whe drawer is
+				// open I am not going to put anything here)
+			}
+
+			@Override
+			public void onDrawerClosed(View drawerView) {
+				super.onDrawerClosed(drawerView);
+				// Code here will execute once drawer is closed
+			}
+
+
+
+		}; // Drawer Toggle Object Made
+		Drawer.setDrawerListener(mDrawerToggle); // Drawer Listener set to the Drawer toggle
+		mDrawerToggle.syncState();               // Finally we set the drawer toggle sync State
+
+
 		//On récupère tous les boutons du menu
 		btn_local = (Button) findViewById(R.id.btn_internal);
 		btn_sd = (Button) findViewById(R.id.btn_sd);
@@ -67,8 +130,29 @@ public class Menu_activity extends ActionBarActivity {
 		btn_videos.setOnClickListener(videosListener);
 		btn_contacts.setOnClickListener(contactsListener);
 
-
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
 	
 	
